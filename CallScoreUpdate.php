766601d44de37e6394ca0a2f25726685e3d954ad
Diwane <adiwane@medline.com>
2019-03-15 14:07:17 +0530

    changing main tbl width to 80 (from 90) and td to 70(from 72)

--- CallScoreUpdate.php
+++ CallScoreUpdate.php
@@ -1,28 +1,27 @@
 <?
 
-
 ?>
 <html>
 <head>
 </head>
 <body>
-<table align="center" width=90%>
+<table align="center"  width=80%>
   <Form name="frmreg" method="post" action="scoreupdate.php">
     <tr>
         <td align="right" width="28%">LeagueName :</td>
-        <td width="72%"><input type="text" name="leaguename" value="" size="30"></td>
+        <td width="70%"><input type="text" name="leaguename" value="" size="30"></td>
     </tr>
     <tr>
       <td align="right" width="28%">MatchId :</td>
-      <td width="72%"><input type="text" name="matchid" value="" size="30"></td>
+      <td width="70%"><input type="text" name="matchid" value="" size="30"></td>
     </tr>
     <tr>
       <td align="right" width="28%">IPL day :</td>
-      <td width="72%"><input type="text" name="iplday" value="" size="30"></td>
+      <td width="70%"><input type="text" name="iplday" value="" size="30"></td>
     </tr>
     <tr>
       <td align="right" width="28%">Is this First time update :</td>
-      <td width="72%"><input type="text" name="firstrun" value="" size="5"></td>
+      <td width="70%"><input type="text" name="firstrun" value="" size="5"></td>
     </tr>
 
     <tr>
