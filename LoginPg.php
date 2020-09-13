80ca1af22e80ef9f2c0eab0d8de968bdd3fe55e9
Diwane <adiwane@medline.com>
2020-09-13 17:39:26 +0530

    shortened the line below title

--- LoginPg.php
+++ LoginPg.php
@@ -5,7 +5,7 @@
     <title>Log In</title>
 
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-  
+
     <style>
         .form-signin {
             width: 100%;
@@ -28,18 +28,24 @@
 <body class="">
     <div class="row m-0">
         <div class="col-12 col-lg-6 col-xl-6 text-center">
-            <h1 class="mt-5 text-danger"> 
-                <small> Welcome to</small> <br> 
+            <h1 class="mt-5 text-danger">
+                <small> Welcome to</small> <br>
                 <strong><em>Fantasy League Cricket </em></strong>
             </h1>
-            <p><strong class="text-success">Create your own league, invite friends, play your own IPL</strong></p>
-            <p class="m-0">
+            <p><strong class="text-success">
+                  Create your own league, with your own rules, play with your own friends,family, office colleagues .. play your own IPL
+            <!--p><strong class="text-success">Create your own league, invite friends, play your own IPL
+            !-->
+        </strong></p>
+          <!--  <p class="m-0">
                 <strong class="text-secondary">
-                    <em> Enjoy IPL even more; Create your own league , 
+                    <em> Enjoy IPL even more; Create your own league ,
                         with your own rules, play with your friends, family, office colleagues...
                     </em>
+
                 </strong>
             </p>
+          !-->
             <form name="frmlogin" class="form-signin bg-primary" action="chklogin.php?action=check" method="post" onSubmit="return validLogin();">
                 <h1 class="h3 mb-3 font-weight-normal text-center text-white">
                     <strong class="text-uppercase">Log In</strong>
@@ -50,10 +56,10 @@
                 <div class="text-center">
                     <input type="submit" class="btn btn-success w-100" value="SIGN IN">
                 </div>
-                <p class="mb-0 mt-3 text-white"> <u>Don't have an account?</u> 
+                <p class="mb-0 mt-3 text-white"> <u>Don't have an account?</u>
                 <a href="teamlogin.php" class="text-danger"> <strong> Register </strong></a></p>
             </form>
-            
+
         </div>
         <div class="col-12 col-lg-6 col-xl-6">
             <div class="row m-0">
@@ -65,7 +71,7 @@
                         <li> To acquire players, League decides to go for bidding process(optional, but lot of fun) OR system can help ‘random allocation’ of players to every team in your league</li>
                         <li>The league creator puts up the draw(schedule of league matches), all of this with just click of button</li>
                         <li>Teams select players, captain based on actual IPL match (based on league draw - can be done in one go for all future matches)</li>
-                        <li>Every team has one-one match with some other league everyday, earns points based on how players performed in actual IPL match</li>
+                        <li>Every team has one-one match with some other team everyday, earns points based on how players performed in actual IPL match</li>
                         <li>Winner/looser, league standing gets decided based on total points</li>
                         <li>Buy, sell players if required - Try it its lot of fun</li>
                         <li>Check this <a href="https://youtu.be/FfUGnFRNtR0" class="text-danger" target="_blank"><strong>video </strong></a> to know more or read this<a href="https://medium.com/@avoodi/owning-an-ipl-team-virtually-9b239801e9bb" class="text-danger" target="_blank"> <strong>blog</strong> </a> to know more</li>
@@ -73,7 +79,7 @@
                 </div>
             </div>
         </div>
-        
+
     </div>
 </body>
 
