01bc95a30ff1a0ee0b05eaa28363b35f19ab1c9a
Diwane <adiwane@medline.com>
2020-09-13 15:17:13 +0530

    newlayout

--- LoginPg.php
+++ LoginPg.php
@@ -1,123 +1,80 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Sign In</title>
-<!-- //custom-theme -->
-<link href="NewUI/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-<link href="NewUI/css/component.css" rel="stylesheet" type="text/css" media="all" />
-<link href="NewUI/css/style_grid.css" rel="stylesheet" type="text/css" media="all" />
-<link href="NewUI/css/style.css" rel="stylesheet" type="text/css" media="all" />
-<!-- font-awesome-icons -->
-<link href="NewUI/css/font-awesome.css" rel="stylesheet">
-<!-- //font-awesome-icons -->
+    <title>Log In</title>
+
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  
+    <style>
+        .form-signin {
+            width: 100%;
+            max-width: 60%;
+            padding: 10px 20px 40px 20px;
+            margin:2% auto ;
+            border-radius: 15px;
+        }
+        .steps{
+            margin: 15% 10% auto;
+            padding: 2%;
+            width: 80%;
+        }
+        .steps-font li{
+            font-size:16px;
+        }
+    </style>
 </head>
-<body>
-			<!-- /pages_agile_info_w3l -->
 
-						<div class="pages_agile_info_w3l">
-							<!-- /login -->
-							<table width="98%" border="0" cellspacing="0" cellpadding="0" align="Center">
-								<tr>
-									<td align="center">
-									<span style="font-size: 20pt;color:#CCCCCC">Welcome to the Fantasy League Cricket </span>
-										</td>
-								</tr>
-								<tr>
-									<td align="center" >
-									<span style="font-size: 15pt;color:#CCCCCC">Create your own league, invite friends, play your own IPL  </span>
-										</td>
-								</tr>
-
-							</table>
-
-							   <div class="over_lay_agile_pages_w3ls">
-								    <div class="registration">
-
-												<div class="signin-form profile">
-													<h2>Sign in Form</h2>
-													<div class="login-form">
-														<form name="frmlogin" action="chklogin.php?action=check" style="margin:0px;" method="post" onSubmit="return validLogin();">
-															<input type="text" name="username" placeholder="Login/E-mail" required="">
-															<input type="password" name="pass" placeholder="Password" required="">
-															<input type="text" name="leaguename" placeholder="League name" required="">
-
-															<div class="tp">
-																<input type="submit" value="SIGN IN">
-															</div>
-														</form>
-													</div>
-													<!-- <div class="login-social-grids">
-														<ul>
-															<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-															<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-															<li><a href="#"><i class="fa fa-rss"></i></a></li>
-														</ul>
-													</div> -->
-													<p><a href="teamlogin.php"> Don't have an account?</a></p>
-
-													 <h6><a href="teamlogin.php">Register</a><h6>
-												</div>
-										</div>
-										<table align="center" width=950>
-											<tr align="center">
-												<td><p>Enjoy IPL even more; Create your own league , with your own rules, play with your friends, family, office colleagues...
-										<p></td>
-											</tr>
-											<tr align="center">
-												<td><strong>Simple steps</strong></td>
-											</tr >
-											<tr align="center">
-												<td>* (Someone) creates a league</td>
-											</tr>
-											<tr align="center">
-												<td>* Friends/family/colleague join that league</td>
-											</tr>
-											<tr align="center">
-												<td>* To acquire players, League decides to go for bidding process(optional, but lot of fun) OR system can help ‘random allocation’ of players to every team in your league
-										</td>
-											</tr>
-											<tr align="center">
-												<td>* The league creator puts up the draw(schedule of league matches), all of this with just click of button </td>
-											</tr>
-											<tr align="center">
-												<td>* Teams select players, captain based on actual IPL match (based on league draw - can be done in one go for all future matches)</td>
-											</tr>
-											<tr align="center">
-												<td align="center">* Every team has one-one match with some other league everyday, earns points based on how players performed in actual IPL match
-											</td>
-											</tr>
-											<tr align="center">
-												<td>* Winner/looser, league standing gets decided based on total points
-											</td>
-											</tr>
-											<tr align="center">
-												<td>* Buy, sell players if required - Try it its lot of fun
-											</td>
-											</tr >
-											<tr align="center">
-												<td>* Check this <a href="https://youtu.be/FfUGnFRNtR0" target="_blank">video</a> to know more or read this<a href="https://medium.com/@avoodi/owning-an-ipl-team-virtually-9b239801e9bb" target="_blank"> blog </a> to know more
-											</td>
-											</tr >
-
-
-										</table>
-
-										<!--copy rights start here-->
-											<div class="copyrights_agile">
-												 <p>© 2017 Esteem. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-											</div>
-											<!--copy rights end here-->
-						    </div>
-						</div>
-
-
-          <script type="text/javascript" src="NewUI/js/jquery-2.1.4.min.js"></script>
-		  <script src="NewUI/js/modernizr.custom.js"></script>
-
-
-
-<script src="NewUI/js/jquery.nicescroll.js"></script>
-<script src="NewUI/js/scripts.js"></script>
-<script type="text/javascript" src="NewUI/js/bootstrap-3.1.1.min.js"></script>
+<body class="">
+    <div class="row m-0">
+        <div class="col-12 col-lg-6 col-xl-6 text-center">
+            <h1 class="mt-5 text-danger"> 
+                <small> Welcome to</small> <br> 
+                <strong><em>Fantasy League Cricket </em></strong>
+            </h1>
+            <p><strong class="text-success">Create your own league, invite friends, play your own IPL</strong></p>
+            <p class="m-0">
+                <strong class="text-secondary">
+                    <em> Enjoy IPL even more; Create your own league , 
+                        with your own rules, play with your friends, family, office colleagues...
+                    </em>
+                </strong>
+            </p>
+            <form name="frmlogin" class="form-signin bg-primary" action="chklogin.php?action=check" method="post" onSubmit="return validLogin();">
+                <h1 class="h3 mb-3 font-weight-normal text-center text-white">
+                    <strong class="text-uppercase">Log In</strong>
+                </h1>
+                <input type="text" name="username" class="form-control mb-2" placeholder="Login/E-mail" required="">
+                <input type="password" name="pass" class="form-control mb-2" placeholder="Password" required="">
+                <input type="text" name="leaguename" class="form-control mb-2" placeholder="League name" required="">
+                <div class="text-center">
+                    <input type="submit" class="btn btn-success w-100" value="SIGN IN">
+                </div>
+                <p class="mb-0 mt-3 text-white"> <u>Don't have an account?</u> 
+                <a href="teamlogin.php" class="text-danger"> <strong> Register </strong></a></p>
+            </form>
+            
+        </div>
+        <div class="col-12 col-lg-6 col-xl-6">
+            <div class="row m-0">
+                <div class="flex steps rounded bg-primary">
+                    <h5 class="text-white"> <strong> <u> Simple steps - </u></strong> </h5>
+                    <ul class="steps-font text-white">
+                        <li>(Someone) creates a league</li>
+                        <li>Friends/family/colleague join that league</li>
+                        <li> To acquire players, League decides to go for bidding process(optional, but lot of fun) OR system can help ‘random allocation’ of players to every team in your league</li>
+                        <li>The league creator puts up the draw(schedule of league matches), all of this with just click of button</li>
+                        <li>Teams select players, captain based on actual IPL match (based on league draw - can be done in one go for all future matches)</li>
+                        <li>Every team has one-one match with some other league everyday, earns points based on how players performed in actual IPL match</li>
+                        <li>Winner/looser, league standing gets decided based on total points</li>
+                        <li>Buy, sell players if required - Try it its lot of fun</li>
+                        <li>Check this <a href="https://youtu.be/FfUGnFRNtR0" class="text-danger" target="_blank"><strong>video </strong></a> to know more or read this<a href="https://medium.com/@avoodi/owning-an-ipl-team-virtually-9b239801e9bb" class="text-danger" target="_blank"> <strong>blog</strong> </a> to know more</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    </div>
 </body>
+
 </html>
---
+++ LoginPgOLD.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sign In</title>
+<!-- //custom-theme -->
+<link href="NewUI/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="NewUI/css/component.css" rel="stylesheet" type="text/css" media="all" />
+<link href="NewUI/css/style_grid.css" rel="stylesheet" type="text/css" media="all" />
+<link href="NewUI/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- font-awesome-icons -->
+<link href="NewUI/css/font-awesome.css" rel="stylesheet">
+<!-- //font-awesome-icons -->
+</head>
+<body>
+	<!-- /pages_agile_info_w3l -->
+	<div class="pages_agile_info_w3l">
+		<!-- /login -->
+		<table width="98%" border="0" cellspacing="0" cellpadding="0" align="Center">
+			<tr>
+				<td align="center">
+				<span style="font-size: 20pt;color:#CCCCCC">Welcome to the Fantasy League Cricket </span>
+					</td>
+			</tr>
+			<tr>
+				<td align="center" >
+				<span style="font-size: 15pt;color:#CCCCCC">Create your own league, invite friends, play your own IPL  </span>
+					</td>
+			</tr>
+
+		</table>
+
+			<div class="over_lay_agile_pages_w3ls">
+				<div class="registration">
+
+							<div class="signin-form profile">
+								<h2>Sign in Form</h2>
+								<div class="login-form">
+									<form name="frmlogin" action="chklogin.php?action=check" style="margin:0px;" method="post" onSubmit="return validLogin();">
+										<input type="text" name="username" placeholder="Login/E-mail" required="">
+										<input type="password" name="pass" placeholder="Password" required="">
+										<input type="text" name="leaguename" placeholder="League name" required="">
+
+										<div class="tp">
+											<input type="submit" value="SIGN IN">
+										</div>
+									</form>
+								</div>
+								<!-- <div class="login-social-grids">
+									<ul>
+										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fa fa-rss"></i></a></li>
+									</ul>
+								</div> -->
+								<p><a href="teamlogin.php"> Don't have an account?</a></p>
+
+									<h6><a href="teamlogin.php">Register</a><h6>
+							</div>
+					</div>
+					<table align="center" width=950>
+						<tr align="center">
+							<td><p>Enjoy IPL even more; Create your own league , with your own rules, play with your friends, family, office colleagues...
+					<p></td>
+						</tr>
+						<tr align="center">
+							<td><strong>Simple steps</strong></td>
+						</tr >
+						<tr align="center">
+							<td>* (Someone) creates a league</td>
+						</tr>
+						<tr align="center">
+							<td>* Friends/family/colleague join that league</td>
+						</tr>
+						<tr align="center">
+							<td>* To acquire players, League decides to go for bidding process(optional, but lot of fun) OR system can help ‘random allocation’ of players to every team in your league
+					</td>
+						</tr>
+						<tr align="center">
+							<td>* The league creator puts up the draw(schedule of league matches), all of this with just click of button </td>
+						</tr>
+						<tr align="center">
+							<td>* Teams select players, captain based on actual IPL match (based on league draw - can be done in one go for all future matches)</td>
+						</tr>
+						<tr align="center">
+							<td align="center">* Every team has one-one match with some other league everyday, earns points based on how players performed in actual IPL match
+						</td>
+						</tr>
+						<tr align="center">
+							<td>* Winner/looser, league standing gets decided based on total points
+						</td>
+						</tr>
+						<tr align="center">
+							<td>* Buy, sell players if required - Try it its lot of fun
+						</td>
+						</tr >
+						<tr align="center">
+							<td>* Check this <a href="https://youtu.be/FfUGnFRNtR0" target="_blank">video</a> to know more or read this<a href="https://medium.com/@avoodi/owning-an-ipl-team-virtually-9b239801e9bb" target="_blank"> blog </a> to know more
+						</td>
+						</tr >
+
+
+					</table>
+
+					<!--copy rights start here-->
+						<div class="copyrights_agile">
+								<p>© 2017 Esteem. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+						</div>
+						<!--copy rights end here-->
+		</div>
+	</div>
+
+
+	<!-- <script type="text/javascript" src="NewUI/js/jquery-2.1.4.min.js"></script>
+	<script src="NewUI/js/modernizr.custom.js"></script>
+
+	<script src="NewUI/js/jquery.nicescroll.js"></script>
+	<script src="NewUI/js/scripts.js"></script>
+	<script type="text/javascript" src="NewUI/js/bootstrap-3.1.1.min.js"></script> -->
+</body>
+</html>
