6afec44c6a377b372fce7566c2beaae27fae4709
Diwane <adiwane@medline.com>
2020-10-05 08:19:48 +0530

    new navbar for dq

---
+++ DQ/DQnavbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-white">
+		<div class="container">
+			<a class="navbar-brand text-danger" href="DQLandingPg.php"> <strong> <em> Fantasy League Cricket </em></strong></a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon text-danger"> Menu </span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link text-danger" href="DQLandingPg.php">DQ Landing</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
