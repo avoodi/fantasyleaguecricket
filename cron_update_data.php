c30e8b6b6d60dd49342958bf1fb908ac974d2bc4
avadhut <a>
2021-04-02 10:54:43 +0530

    we commented most part as it was consuming api hits.. so everyday hardcoded matchid is given

--- cron_update_data.php
+++ cron_update_data.php
@@ -1,6 +1,6 @@
 <?php
 require_once "updatePlayerMatchDetails.php";
-
+/**
 $matches_url="http://cricapi.com/api/matches?&apikey=wEjKd5SIYLgDkKGUlW3LjXk66hE3";
 $matchesRaw = file_get_contents($matches_url);
 $matchesJson = json_decode($matchesRaw, true);
@@ -15,12 +15,29 @@ function yesterdays_ttwenty_matches($match){
 }
 
 $yesterdays_matches=array_filter($matches, "yesterdays_ttwenty_matches");
-
 foreach($yesterdays_matches as $match){
+
  	$matchid=$match["unique_id"];
+
  	echo "Running script for matchid ".$matchid."<br>";
  	run($matchid);
  	echo "script completed for ".$matchid."<br>";
+
 }
+**/
+// This is to be changed everyday till we pass it as param
+$matchid1=1237181;
+//$matchid2=1216530;
+
+echo "Running script for matchid ".$matchid1."<br>";
+run($matchid1);
+echo "script completed for ".$matchid1."<br>";
+echo " **********************************<br>";
+/**
+echo "Running script for matchid ".$matchid2."<br>";
+run($matchid2);
+echo "script completed for ".$matchid2."<br>";
+echo " **********************************<br>";
+**/
 //run(1136608);
 ?>
