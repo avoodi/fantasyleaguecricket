5efe0fadfcdc829c4b3ed2a4c06265380970999e
avoodi <avoodi@gmail.com>
2021-04-11 16:04:52 +0530

    checked for dummy

--- DQ/DQteamregister.php
+++ DQ/DQteamregister.php
@@ -26,7 +26,7 @@ $blankgroupname=0;
 if (isset($_POST['selGroupName']) )
 {
 	$groupname = $_POST['selGroupName'];
-	if($groupname=='') { // we are checking this in DQteamlogin so pl remove it after few days
+	if($groupname=='dummy') { // we are checking this in DQteamlogin so pl remove it after few days
 		$blankgroupname=1;
 		echo '<script type="text/javascript">alert("Oops, looks like you did a mistake in selecting/entering leaguename; Lets try doing it again."); window.location = "./DQteamlogin.php" </script>';
 	}
@@ -66,7 +66,7 @@ else {
 }
 
 //echo " the session vars are  " . $_SESSION['teamname'] . " and " . $_SESSION['leaguename']  ."<br/>" ;
-if ($existinggroup=="no" && $count==0){
+if ($existinggroup=="no" && $count==0 && groupname !='dummy'){
 // this means its a new league - and then we have to set it up by inserting into 3 tables
 // lets first set this user as leaguecreator
 $_SESSION['groupname']=$groupname;
