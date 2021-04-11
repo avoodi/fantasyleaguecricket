95f3e71b2ebf9804433ac1a4ac67c4bac4c37004
avoodi <avoodi@gmail.com>
2021-04-11 12:33:45 +0530

    changed error alert msg

--- DQ/DQteamregister.php
+++ DQ/DQteamregister.php
@@ -26,7 +26,7 @@ if (isset($_POST['selGroupName']) )
 {
 	$groupname = $_POST['selGroupName'];
 	if($groupname=='') {
-		echo '<script type="text/javascript">alert("Oops, looks like you did a mistake in selecting/entering leaguename; Please hit refresh on browser and start again."); window.location = "./DQteamlogin.php" </script>';
+		echo '<script type="text/javascript">alert("Oops, looks like you did a mistake in selecting/entering leaguename; Lets try doing it again."); window.location = "./DQteamlogin.php" </script>';
 	}
 	$_SESSION['groupname']=$groupname;
 	// if user selected existing league then stop him from creating team with same name
